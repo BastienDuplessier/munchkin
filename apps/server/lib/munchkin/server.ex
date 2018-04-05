4ce4ae01086b9d322aff5745bacce5cfee70caf9
Bastien Duplessier <bastien.duplessier@gmail.com>
2018-04-05 02:27:16 +0200

    Add login call

--- apps/server/lib/munchkin/server.ex
+++ apps/server/lib/munchkin/server.ex
@@ -9,8 +9,24 @@ defmodule Munchkin.Server do
     GenServer.start_link(__MODULE__, %{}, options)
   end
 
+  def handle_call({:login, name}, {pid, _ref}, state) do
+    {response, new_state} = login(state, name, pid)
+    {:reply, response, new_state}
+  end
+
   def handle_call(_, _, state) do
     {:reply, {:err, "not found"}, state}
+  end
+
+  defp login(state, name, from) do
+    case Map.fetch(state, name) do
+      {:ok, ^from} ->
+        {{:err, "Already logged in"}, state}
+      {:ok, _} ->
+        {{:err, "Name already taken"}, state}
+      _ ->
+        {:ok, Map.put(state, name, from)}
+    end
   end
 
 end
